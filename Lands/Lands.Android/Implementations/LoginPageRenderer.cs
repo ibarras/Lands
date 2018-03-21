306e510cffce0a37f0a2d8c5ff7f15ecba1f8a5d
Cayman01 <cayman01.filiales@gmail.com>
2018-03-20 22:13:25 -0500

    FBLogin

--- Lands/Lands.Android/Implementations/LoginPageRenderer.cs
+++ Lands/Lands.Android/Implementations/LoginPageRenderer.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using Android.App;
-using Newtonsoft.Json;
-using System;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Xamarin.Auth;
-using Xamarin.Forms;
-using Xamarin.Forms.Platform.Android;
-
-[assembly: ExportRenderer(typeof(LoginFacebookPage), typeof(Soccer.Droid.LoginPageRenderer))]
+﻿[assembly: Xamarin.Forms.ExportRenderer(
+    typeof(Lands.Views.LoginFacebookPage),
+    typeof(Lands.Droid.Implementations.LoginPageRenderer))]
 
 namespace Lands.Droid.Implementations
 {
+    using System;
+    using System.Threading.Tasks;
+    using Android.App;
+    using Models;
+    using Services;
+    using Xamarin.Auth;
+    using Xamarin.Forms.Platform.Android;
+
     public class LoginPageRenderer : PageRenderer
     {
         public LoginPageRenderer()
@@ -19,7 +19,7 @@ namespace Lands.Droid.Implementations
             var activity = this.Context as Activity;
 
             var auth = new OAuth2Authenticator(
-                clientId: "1449461771828793",
+                clientId: "1263595770461023",
                 scope: "email",
                 authorizeUrl: new Uri("https://www.facebook.com/dialog/oauth/"),
                 redirectUrl: new Uri("https://www.facebook.com/connect/login_success.html"));
@@ -43,11 +43,8 @@ namespace Lands.Droid.Implementations
 
         private async Task<FacebookResponse> GetFacebookProfileAsync(string accessToken)
         {
-            var requestUrl = "https://graph.facebook.com/v2.8/me/?fields=name,picture.width(999),cover,age_range,devices,email,gender,is_verified,birthday,languages,work,website,religion,location,locale,link,first_name,last_name,hometown&access_token=" + accessToken;
-            var httpClient = new HttpClient();
-            var userJson = await httpClient.GetStringAsync(requestUrl);
-            var facebookResponse = JsonConvert.DeserializeObject<FacebookResponse>(userJson);
-            return facebookResponse;
+            var apiService = new ApiService();
+            return await apiService.GetFacebook(accessToken);
         }
     }
 }
